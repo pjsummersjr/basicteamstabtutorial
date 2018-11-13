1044b9354a127d2ab6e0bc7c1d1838ccf2259bdc
Paul Summers <paulsumm@microsoft.com>
2018-11-12 23:06:14 -0500

    Searchable with API

--- TabsDemo/scripts/app.js
+++ TabsDemo/scripts/app.js
@@ -1,3 +1,29 @@
 ﻿$(document).ready(function () {
-    $('#name').text('Paul Summers')
+    
+    microsoftTeams.initialize();
+    microsoftTeams.getContext(function (context) {
+        $('#name').text(`Welcome, ${context.upn}`);
+    });
 });
+
+function searchClick() {
+    var input = $('#searchTerms').val();
+    var request = $.get({
+            url: `https://api.cognitive.microsoft.com/bing/v7.0/news/search`,
+            crossDomain: true,
+            headers: {
+                "Ocp-Apim-Subscription-Key":"7b8b0e1bc1e74db48f3389ba2ce84ff3"
+            }
+        })
+        .done(function (data) {
+            let resultsHtml = '<ul class="list-group">';
+            data.value.map((object, i) => {
+                resultsHtml += `<li class="list-group-item"><a href="${object.url}">${object.name}</a><br/>${object.description}</li>`;
+            })
+            resultsHtml += '</ul>';
+            $('#results').html(resultsHtml);
+        })
+        .fail(function () {
+            $('#results').text(`Couldn't load search results. Sorry.`);
+        })
+}
